850bc36e3d458a062bcd1eae6ac80a4a03f535ac
Vladimir Golovchuk <psyghogift@gmail.com>
2022-10-16 22:38:09 +0300

    feat: added some tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,11 +15,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let result = ""
+  let [repeatTimes, separator, addition, additionRepeatTimes, additionSeparator] = Object.values(options)
+  additionRepeatTimes = additionRepeatTimes === undefined ? 0 : additionRepeatTimes
+  const fullRepeatTimes = repeatTimes + additionRepeatTimes
+  additionSeparator = additionSeparator === undefined ? "" : additionRepeatTimes
+  const fullSeparator = separator + additionSeparator
+  for (let i = 0; i < fullRepeatTimes; i++) {
+    result += fullSeparator;
+  }
+  return result
 }
+console.log(repeater('la', { repeatTimes: 3 }), 'la+la+la');
+console.log(repeater('single', { repeatTimes: 1 }), 'single');
+console.log(repeater('12345', { repeatTimes: 5 }), '12345+12345+12345+12345+12345');
 
+console.log(repeater('la', { repeatTimes: 3, separator: 's' }), 'laslasla');
+console.log(repeater('point', { repeatTimes: 3, separator: '&&&' }), 'point&&&point&&&point');
+console.log(repeater('12345', { repeatTimes: 5, separator: '3 words separator' }), '123453 words separator123453 words separator123453 words separator123453 words separator12345');
 module.exports = {
   repeater
 };
